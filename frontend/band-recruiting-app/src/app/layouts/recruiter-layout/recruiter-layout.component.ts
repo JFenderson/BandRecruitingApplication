a946e6d7638b11fa12cbedf4c0e611b78133de83
JFenderson <fenderson.joseph@gmail.com>
2025-05-17 16:24:27 -0500

    adding layout components

---
+++ frontend/band-recruiting-app/src/app/layouts/recruiter-layout/recruiter-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recruiter-layout',
+  imports: [],
+  templateUrl: './recruiter-layout.component.html',
+  styleUrl: './recruiter-layout.component.scss'
+})
+export class RecruiterLayoutComponent {
+
+}
